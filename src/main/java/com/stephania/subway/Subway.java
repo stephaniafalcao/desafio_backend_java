5f1a7133c425a2b483447965afe20ed3411d1da7
Stephania Falcão do Nascimento <stephaniafalcao35025@gmail.com>
2017-08-13 17:22:10 -0300

    Criadas classes Direction e Subway

---
+++ src/main/java/com/stephania/subway/Subway.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stephania.subway;
+
+/**
+ *
+ * @author stephania
+ */
+public class Subway {
+    private int x, y, z;
+    private Direction direction;
+    
+    // Todo submarino começa com as coordenadas (0,0,0,North)
+    public Subway() {
+        x = 0;
+        y = 0;
+        z = 0;
+        
+        direction = Direction.NORTH;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the z
+     */
+    public int getZ() {
+        return z;
+    }
+
+    /**
+     * @param z the z to set
+     */
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    /**
+     * @return the direction
+     */
+    public Direction getDirection() {
+        return direction;
+    }
+
+    /**
+     * @param direction the direction to set
+     */
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    
+    
+}
